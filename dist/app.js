09697b4d0832fd96dd7dea553b7389a61df1ddda
DESKTOP-U096J61\dhaip <brookemmcburneym@gmail.com>
2026-05-02 13:35:23 +0600

    start practice  module one

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = 'kolim';
+const age = 67;
+let isPassed = true;
+console.log(name, age, isPassed);
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+let name: string = 'kolim';
+const age: number = 67; 
+let isPassed: boolean = true
+// console.log(name, age, isPassed)
+
+let score: number[] = [45, 56, 34, 56, 98];
+let playear: string[] = ['dippo', 'kolim', 'bosundhora']
+console.log(score, playear);
